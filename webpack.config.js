5ab8e76ee334d7da1b46fef275c4a4745e06fef5
Cillié Malan <cilliemalan@gmail.com>
2018-02-23 16:40:16 +0200

    Refactored webpack config and split between dev and prod

--- webpack.config.js
+++ webpack.config.js
@@ -7,23 +7,35 @@ const config = require('./config');
 const dist = path.resolve(__dirname, 'public');
 const now = new Date().toISOString().replace(/[:.ZT-]/g,'');
 
+const plugins = [
+    new HtmlWebpackPlugin({ template: 'app/index.ejs', gaTrackingId: config.gaTrackingId }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+        environment: config.environment,
+        recaptchaKey: config.recaptchaKey,
+        clientId: config.clientId
+    })
+];
+
+const app = [
+    './app/index.js'
+];
+
+if (config.production) {
+    plugins.push(new MinifyPlugin());
+} else {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+
+    app.splice(0, 0, 'react-hot-loader/patch');
+    app.push('webpack-hot-middleware/client');
+}
+
 module.exports = {
     entry: {
-        app: [
-            'react-hot-loader/patch',
-            './app/index.js',
-            'webpack-hot-middleware/client'
-        ]
+        app: app
     },
     resolve: { extensions: ['.js', '.jsx'] },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'app/index.ejs'
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new MinifyPlugin()
-    ],
+    plugins: plugins,
     module: {
         rules: [
             {
