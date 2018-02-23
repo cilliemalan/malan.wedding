e9ffcd95f0911ae682db9d818d70e97fa9eb9a99
Cillié Malan <cilliemalan@gmail.com>
2018-02-23 13:54:42 +0200

    Webpack middleware

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ const { execSync } = require('child_process');
 const output = execSync('yarn');
 console.log(output.toString());
 
-
 // module requires
 const express = require('express');
 const winston = require('winston');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
 
 // local requires
 const webhooks = require('./webhooks');
 const api = require('./api');
+const webpackconfig = require('./webpack.config');
 
 // env
 const port = process.env.PORT || 3000;
@@ -27,13 +32,33 @@ winston.info(`loglevel: ${loglevel}`);
 winston.info('port: %s', port);
 winston.info('github hook secret: %s', !!ghsecret);
 
-
 // express app
 const app = express();
 
-
 // set up pipeline
+// API
 app.use('/api', api);
+
+// static files
 app.use(express.static('public'));
+
+// webhooks
 app.use('/webhooks', webhooks(ghsecret));
+
+// webpack
+const webpackCompiler = webpack(webpackconfig);
+const wpmw = webpackMiddleware(webpackCompiler, {});
+const wphmw = webpackHotMiddleware(webpackCompiler);
+app.use(wpmw);
+app.use(wphmw);
+
+// SPA
+app.use((req, res) => {
+    const indexFile = `${webpackconfig.output.path}/index.html`;
+    const index = webpackCompiler.outputFileSystem.readFileSync(indexFile);
+    res.contentType('text/html');
+    res.end(index);
+});
+
+
 app.listen(port, () => winston.info(`Listening on port ${port}!`));
