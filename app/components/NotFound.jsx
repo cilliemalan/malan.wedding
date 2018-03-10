276d5afc573d5c9908ba93e9374d8888585099b8
Cillié Malan <cilliemalan@gmail.com>
2018-03-10 19:44:25 +0200

    Fixed not found

--- app/components/NotFound.jsx
+++ app/components/NotFound.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-export const Home = (props) =>
-    <div>
+export const NotFound = (props) =>
+    <div className="notfound">
+        404 <br />
         Not Found
+        <p><Link to="/">Go Back</Link></p>
     </div>
