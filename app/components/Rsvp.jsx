d1eadbf80a18dad48737d9e3e0ffcb4d89b50254
Cillié Malan <cilliemalan@gmail.com>
2018-03-10 22:03:46 +0200

    Much more complete rsvp thing

--- app/components/Rsvp.jsx
+++ app/components/Rsvp.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 
-
 export class Rsvp extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+
+        // bad I know
+        const { email, description, guestlist } = props.person || {};
 
         this.state = {
             errors: {},
+            name: description,
+            email: email,
+            num: guestlist && `${guestlist.length} people`,
+            special: !!email,
+            guests: guestlist
         };
 
         this.clearForm = this.clearForm.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
         this.onInputChange = this.onInputChange.bind(this);
     }
+
+
 
     clearForm() {
         this.setState(() => { errors: { } });
@@ -26,40 +35,133 @@ export class Rsvp extends React.Component {
         this.setState({ [e.target.name]: e.target.value });
     }
 
-    render() {
+    internals() {
         const errorClass = (element) => this.state.errors[element] ? 'error' : '';
+        const { special, name, email, num, guests } = this.state;
 
-        return <div className="rsvp">
-            <h1>RSVP</h1>
-            <form id="rsvpForm" onSubmit={this.onSubmit} ref={(f) => { this._form = f; }}>
-                <div id="group">
-                    <input type="text"
-                        value={this.state.name}
-                        className={`${errorClass('name')}`}
-                        name="name"
-                        placeholder="Name &amp; Surname"
-                        onChange={this.onInputChange}
-                        onFocus={this._resolveFocus} />
+        if (special) {
+            return <div className='rsvpspecial'>
+                <p className="note">Please indicate attendance for the Ceremony</p>
+                <div className="form">
+                    <div className="section1">
+                        <label className="group">
+                            Name
+                        </label>
+                        <label className="group">
+                            Email
+                        </label>
+                        <label className="group num">
+                            # people
+                        </label>
+                        <div className="group">
+                            <input type="text"
+                                value={name}
+                                className={`${errorClass('name')}`}
+                                name="name"
+                                placeholder="Name &amp; Surname"
+                                onChange={this.onInputChange}
+                                onFocus={this._resolveFocus} />
+                        </div>
+                        <div className="group">
+                            <input type="text"
+                                className={`${errorClass('email')}`}
+                                value={email}
+                                name="email"
+                                placeholder="my@email.com"
+                                onChange={this.onInputChange} />
+                        </div>
+                        <div className="group num">
+                            <input type="text"
+                                className={`${errorClass('num')}`}
+                                value={num}
+                                name="num"
+                                placeholder="# people"
+                                onChange={this.onInputChange} />
+                        </div>
+                    </div>
+                    <h2>Dinner RSVP</h2>
+                    <p className="note">Please indicate attendance for the Dinner Receiption</p>
+                    <ul className="section2">
+                        {guests.map((guest, i) => <li key={i}>
+                            <input type="checkbox" id={`chk-${i}`} />
+                            <label htmlFor={`chk-${i}`}>{guest}</label>
+                        </li>)}
+                    </ul>
+                    <div className="group submit">
+                        <button ref={(me) => { this._button = me; }} >
+                            Submit
+                        </button>
+                    </div>
                 </div>
-                <div id="group">
-                    <input type="text"
-                        className={`${errorClass('email')}`}
-                        value={this.state.email}
-                        name="email"
-                        placeholder="my@email.com"
-                        onChange={this.onInputChange} />
-                </div>
-                <div id="group">
-                    <button ref={(me) => { this._button = me; }} >
-                        Submit
-                    </button>
-                </div>
-            </form>
-            <div className='smallprint'>
+                <div className='smallprint'>
                     <a href="/terms_and_conditions.html" target="_blank">Terms&nbsp;&amp;&nbsp;Conditions</a>
-                &nbsp;and&nbsp;
+                    &nbsp;and&nbsp;
                     <a href="/privacy_policy.html" target="_blank">Privacy&nbsp;Policy</a>.
                 </div>
-        </div>;
+            </div>;
+        } else {
+            return <div className='rsvp'>
+                <div className="form">
+                    <div className="group">
+                        <input type="text"
+                            value={name}
+                            className={`${errorClass('name')}`}
+                            name="name"
+                            placeholder="Name &amp; Surname"
+                            onChange={this.onInputChange}
+                            onFocus={this._resolveFocus} />
+                    </div>
+                    <div className="group">
+                        <input type="text"
+                            className={`${errorClass('email')}`}
+                            value={email}
+                            name="email"
+                            placeholder="my@email.com"
+                            onChange={this.onInputChange} />
+                    </div>
+                    <div className="group num">
+                        <input type="text"
+                            className={`${errorClass('num')}`}
+                            value={num}
+                            name="num"
+                            placeholder="# people"
+                            onChange={this.onInputChange} />
+                    </div>
+                    <div className="group submit">
+                        <button ref={(me) => { this._button = me; }} >
+                            Submit
+                        </button>
+                    </div>
+                </div>
+                <div className='smallprint'>
+                    <a href="/terms_and_conditions.html" target="_blank">Terms&nbsp;&amp;&nbsp;Conditions</a>
+                    &nbsp;and&nbsp;
+                        <a href="/privacy_policy.html" target="_blank">Privacy&nbsp;Policy</a>.
+                </div>
+            </div>;
+        }
+    }
+
+    yesno() {
+        return <div>
+            <p className="note">Will you be joining us on the 13th of April?</p>
+            <div className="yesno">
+                <button className="yes" onClick={(e) => { e.preventDefault(); this.setState((state) => ({...state, yesno: "yes"})); } }>YES!</button>
+                <button className="no" type="submit">Unfortunately not</button>
+            </div>
+        </div>
+    }
+
+    render() {
+        const { yesno } = this.state;
+
+        return <div className="rsvp-outer">
+            <h1>RSVP</h1>
+            <form method="POST" action="/api/rsvp/">
+                <input type="hidden" name="going" value={yesno || "no"} />
+                <input type="hidden" name="email" value={this.state.email} />
+                {yesno ? this.internals() : this.yesno()}
+            </form>
+        </div>
     }
 }
