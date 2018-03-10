22ce80f2d5abbd628641275bb39da4de761c51bf
Cillié Malan <cilliemalan@gmail.com>
2018-03-10 22:58:59 +0200

    Fixed home page rsvp

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { Invite } from './Invite';
 import { Registries } from './Registries';
 import { Flasher } from './Flasher';
-import { Rsvp } from './Rsvp';
+import { Link } from 'react-router-dom';
 
 export const Home = (props) =>
     <div>
         <Flasher />
         <Invite />
-        <Rsvp />
+        <div className="rsvp-block">
+        <h1>RSVP</h1>
+            <p className="note">
+                The ceremony is open for anyone who wants to join us!
+            </p>
+            <Link className="rsvp-button" to="/rsvp/">RSVP</Link>
+        </div>
         <Registries />
     </div>
