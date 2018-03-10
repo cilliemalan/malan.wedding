34728b7760636950b5aa3d782332676aa33dddca
Cillié Malan <cilliemalan@gmail.com>
2018-03-10 19:20:43 +0200

    Added some RSVP stuff

--- app/components/Rsvp.jsx
+++ app/components/Rsvp.jsx
@@ -19,7 +19,7 @@ export class Rsvp extends React.Component {
     }
 
     onSubmit() {
-        
+
     }
 
     onInputChange(e) {
@@ -29,6 +29,37 @@ export class Rsvp extends React.Component {
     render() {
         const errorClass = (element) => this.state.errors[element] ? 'error' : '';
 
-        return <div className="rsvp"><h1>RSVP</h1></div>
+        return <div className="rsvp">
+            <h1>RSVP</h1>
+            <form id="rsvpForm" onSubmit={this.onSubmit} ref={(f) => { this._form = f; }}>
+                <div id="group">
+                    <input type="text"
+                        value={this.state.name}
+                        className={`${errorClass('name')}`}
+                        name="name"
+                        placeholder="Name &amp; Surname"
+                        onChange={this.onInputChange}
+                        onFocus={this._resolveFocus} />
+                </div>
+                <div id="group">
+                    <input type="text"
+                        className={`${errorClass('email')}`}
+                        value={this.state.email}
+                        name="email"
+                        placeholder="my@email.com"
+                        onChange={this.onInputChange} />
+                </div>
+                <div id="group">
+                    <button ref={(me) => { this._button = me; }} >
+                        Submit
+                    </button>
+                </div>
+            </form>
+            <div className='smallprint'>
+                    <a href="/terms_and_conditions.html" target="_blank">Terms&nbsp;&amp;&nbsp;Conditions</a>
+                &nbsp;and&nbsp;
+                    <a href="/privacy_policy.html" target="_blank">Privacy&nbsp;Policy</a>.
+                </div>
+        </div>;
     }
 }
