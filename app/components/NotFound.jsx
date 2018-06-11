a6f4cb49dfef1543401df7ad9caa8ef12416abee
Cillié Malan <cilliemalan@gmail.com>
2018-06-11 12:50:33 +0200

    Removed the application

--- app/components/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-
-export const NotFound = (props) =>
-    <div className="notfound">
-        404 <br />
-        Not Found
-        <p><Link to="/">Go Back</Link></p>
-    </div>
--- app/components/SaveTheDate.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const SaveTheDate = (props) =>
-    <div>
-        <img src="savethedate.jpg" id="save_the_date" alt="Save the Date! Friday 13 April" />
-    </div>
-
